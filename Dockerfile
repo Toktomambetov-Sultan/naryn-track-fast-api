8af527ef70b08984b5bb63a576226951a3973ac6
Toktomambetov-Sultan <sultan.toktomambetov.04@bk.ru>
2025-04-07 00:09:26 +0600

    created deploy

--- Dockerfile
+++ Dockerfile
@@ -3,12 +3,15 @@ FROM python:3.10
 WORKDIR /app
 
 COPY requirements.txt .
-COPY .env .env
+COPY .env .
 
 RUN pip install --no-cache-dir -r requirements.txt
 
+ENV PYTHONPATH=/app
+
 COPY . .
 
-EXPOSE 8000
 
-CMD ["uvicorn", "app.main:app", "--host", "0.0.0.0", "--port", "8000"]
+EXPOSE 8100
+
+CMD ["sh", "-c", "alembic upgrade head && uvicorn main:app --host 0.0.0.0 --port 8100"]
--- app/database.py
+++ app/database.py
@@ -6,13 +6,14 @@ import os
 load_dotenv()
 
 # Get individual DB components
-DB_HOST = os.getenv("DB_HOST", "localhost")
-DB_PORT = os.getenv("DB_PORT", "5432")
-DB_NAME = os.getenv("DB_NAME", "postgres")
-DB_USER = os.getenv("DB_USER", "postgres")
-DB_PASSWORD = os.getenv("DB_PASSWORD", "postgres")
+DB_HOST = os.getenv("DB_HOST")
+DB_PORT = os.getenv("DB_PORT")
+DB_NAME = os.getenv("DB_NAME")
+DB_USER = os.getenv("DB_USER")
+DB_PASSWORD = os.getenv("DB_PASSWORD")
 
 DATABASE_URL = f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+print(DATABASE_URL)
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
